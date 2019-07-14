02e17e25b875aaa480aa0cf415cc905432651b00
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-07-14 01:59:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
     quantity.times do
     items << title
   end
-  
+  @last_transaction = 
   end
   
   def apply_discount
@@ -26,7 +26,7 @@ class CashRegister
   end
   
   def void_last_transaction
-    self.total = self.total - self.last_transaction
+    @total = @total - @last_transaction
   end
   
   
