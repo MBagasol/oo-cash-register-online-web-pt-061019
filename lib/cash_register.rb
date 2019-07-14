006a1d4a3cc141d69597fccfdad8220f7a63ed58
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-07-14 01:52:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,10 @@
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items
   
   def initialize(discount = 0) 
     @total = 0 
     @discount = discount
+    @items = []
   end
   
   def add_item(title, price, quantity = 1)
@@ -21,7 +22,8 @@ class CashRegister
   end
   
   def items 
-    
+    @items
+  end
   
   
   
